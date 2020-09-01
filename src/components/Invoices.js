3022074bd253b734879ed3193cfb976c12b4d94b
mattenbar <matthw.enbar@gmail.com>
2020-09-01 19:15:22 -0400

    displays name of customers and description in link to invoices

--- src/components/Invoices.js
+++ src/components/Invoices.js
@@ -9,16 +9,27 @@ class Invoices extends Component {
  
   
   render(){
+    let c
+    let links
+    let customers
+    if(this.props.customers.length > 0 ) {
+      links = this.props.invoices.map(invoice => {
+      customers = this.props.customers.map(customer => customer.attributes)
+      c = customers.find(customer => customer.id === invoice.attributes.customer_id)
+      return <Link key={invoice.id} to={`/invoices/${invoice.id}`}>Invoice #{invoice.id} - {c.name} - {invoice.attributes.description}<br/></Link> 
+      }
+      )
     return (
        <div>
        <ul>
-        {this.props.invoices.map(invoice => 
-          <Link key={invoice.id} to={`/invoices/${invoice.id}`}>Invoice #{invoice.id} - {invoice.attributes.customer_id}<br/></Link> 
-        )}
+        {links}
         </ul>
        </div>
       
-    )
+    )}
+    else{
+      return (<div></div>)
+    }
   }
 }
 

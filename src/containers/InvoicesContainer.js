92d20dcb00a68cf43ae90cde4b81bd9ded6782bf
mattenbar <matthw.enbar@gmail.com>
2020-09-01 08:58:42 -0400

    changed button to bootsrap button on invoice container

--- src/containers/InvoicesContainer.js
+++ src/containers/InvoicesContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Invoices from '../components/Invoices'
 import InputInvoice from '../components/InputInvoice'
+import Button from 'react-bootstrap/Button'
 
 
 class InvoicesContainer extends React.Component {
@@ -11,15 +12,17 @@ class InvoicesContainer extends React.Component {
     return (
       <div>
         <h1>Invoices:</h1>
-        <ul><button
-          onClick={() =>
-            this.setState((prevState) => ({
-              newInvoice: prevState.newInvoice === "true" ? "false" : "true"
-            }))
-          }
-        >
-          Create New Invoice
-        </button></ul>
+        <ul>
+          <Button
+            onClick={() =>
+              this.setState((prevState) => ({
+                newInvoice: prevState.newInvoice === "true" ? "false" : "true"
+              }))
+            }
+          >
+            Create New Invoice
+          </Button>
+        </ul>
         {this.state.newInvoice === "true" ? (
         <div>
           <ul><InputInvoice /></ul>
